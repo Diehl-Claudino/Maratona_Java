63b35e4ffd2986d3b99e90feea6a7b091ca74e68
Magdiel <claudinobusiness@gmail.com>
2021-08-22 22:31:39 -0300

    Create Exercicio2.java

---
+++ src/EstudoMaratonaJava/Exercicio2.java
@@ -0,0 +1,20 @@
+package EstudoMaratonaJava;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        double salarioAnual = 70000;
+        double primeiraFaixa = 9.50/100;
+        double segundaFaixa = 37.35/100;
+        double terceiraFaixa = 49.50/100;
+        double valorImposto;
+
+        if (salarioAnual <=34712){
+            salarioAnual = salarioAnual*primeiraFaixa;
+        }else if (salarioAnual >=34713 &&salarioAnual <=68507){
+            salarioAnual = salarioAnual*segundaFaixa;
+        }else{
+            salarioAnual=salarioAnual*terceiraFaixa;
+        }
+        System.out.println(salarioAnual);
+    }
+}
