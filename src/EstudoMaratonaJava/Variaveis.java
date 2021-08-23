9f53d28e6a2ee8e7ffdea9b58a7bacd37e2a1a67
Magdiel <claudinobusiness@gmail.com>
2021-08-22 22:30:44 -0300

    Create Variaveis.java

---
+++ src/EstudoMaratonaJava/Variaveis.java
@@ -0,0 +1,28 @@
+package EstudoMaratonaJava;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean (tipos primitivos)
+        int age = 10;
+        long nemeroGrande = 1000000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+
+        String nome = "Diehl";
+
+        System.out.println("A idade é de: "+age + " anos");
+        System.out.println("O Salario é de: "+salarioDouble + " reais");
+        System.out.println("O Salario é de: "+salarioFloat + " reais");
+        System.out.println("A Idade é de: "+idadeByte + " reais");
+        System.out.println("A Idade é de: "+idadeShort + " reais");
+        System.out.println(verdadeiro);
+        System.out.println(falso);
+        System.out.println(" Sexo: "+caractere);
+        System.out.println( " Oi meu nome é" +nome);
+    }
+}
