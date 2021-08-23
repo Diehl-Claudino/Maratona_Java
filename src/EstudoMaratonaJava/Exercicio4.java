ea894eae57083de50d40b42ad7022dfaa67bf513
Magdiel <claudinobusiness@gmail.com>
2021-08-22 22:31:27 -0300

    Create Exercicio4.java

---
+++ src/EstudoMaratonaJava/Exercicio4.java
@@ -0,0 +1,12 @@
+package EstudoMaratonaJava;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        //imprima todos os números pares de 0 até 100
+        for(int i =1; i<100; i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
